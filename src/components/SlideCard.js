6846894a1c02f587a58b4e46b9c2ca00411902f5
yuki <mansie.torsholt@gmail.com>
2021-06-13 00:14:37 +0200

    turn client quotes into slider

--- src/components/SlideCard.js
+++ src/components/SlideCard.js
@@ -3,18 +3,18 @@ import { Card, Button } from 'react-bootstrap';
 
 
 
-const slideCard = (cases) => {
+const slideCard = ({cases}) => {
   console.log(cases);
   return (
     <div>
       <Card style={{ width: '16rem', height: '22rem', boxShadow:"0 0 1rem 0.2rem lightgrey" }}>
-        <Card.Img variant="top" src={cases.cases.image} style={{height: '8rem', objectFit: 'cover'}}/>
+        <Card.Img variant="top" src={cases.image} style={{height: '8rem', objectFit: 'cover'}}/>
         <Card.Body>
-          <Card.Title>{cases.cases.name}</Card.Title>
+          <Card.Title>{cases.name}</Card.Title>
           <Card.Text>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, sint?
           </Card.Text>
-          <Button variant="primary">Go to {cases.cases.name}</Button>
+          <Button variant="primary">Go to {cases.name}</Button>
         </Card.Body>
       </Card>
     </div>
